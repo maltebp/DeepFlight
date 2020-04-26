0f8b1afec4c3e5580fa98000adba09a27749b477
ErlendTyrmi <erlend@tyrmi.com>
2020-04-26 22:28:52 +0200

    form style

--- WebSite/src/Download.js
+++ WebSite/src/Download.js
@@ -5,7 +5,7 @@ class Download extends Component {
   render() {
     return (
       <div>
-        <h2>DOWNLOAD THE GAME!</h2>
+        <h2>LOGIN TO DOWNLOAD THE GAME!</h2>
         <div className="boxwrapper">
         <div className="box">
         <Login/>
