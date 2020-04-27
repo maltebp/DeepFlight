5892dbb07bfaa58a04a39a607a23802d121bdee5
Erlend Tyrmi <erlend@tyrmi.com>
2020-04-27 23:30:40 +0200

    Web update
    * http in address cors
    
    * form style
    
    * autocommit
    
    * autocommit
    
    * token received and stored
    
    * removed form warning controlled field

--- WebSite/src/Download.js
+++ WebSite/src/Download.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import axios from 'axios';
+import jwt from 'jsonwebtoken';
 
 class Download extends Component {
   render() {
     return (
       <div>
-        <h2>DOWNLOAD THE GAME!</h2>
+        <h2>LOGIN TO DOWNLOAD THE GAME!</h2>
         <div className="boxwrapper">
         <div className="box">
         <Login/>
@@ -46,21 +47,28 @@ class Login extends Component {
 
     axios({
       method: 'post',
-      url: 'http://maltebp.dk:7000/login', 
+      url: 'http://maltebp.dk:7000/login',
+      //url: 'http://localhost:7000/login',
       data: bodyFormData,
       headers: {
         'Content-Type': 'multipart/form-data',
         'Access-Control-Allow-Origin': 'http://maltebp.dk:7000/login'
+        //'Access-Control-Allow-Origin': 'http://localhost:7000'
       },
       withCredentials: true})
 
       .then(response => {
+        // TODO remove these alerts and logs!
         console.log(response);
-        var result = JSON.stringify(response.statusText);
+        const token = response.data;
+        var decoded = jwt.decode(token.jwt);
+        console.log(decoded);
+        var result = JSON.stringify(response.statusText) + "\nJWT:\n" + JSON.stringify(decoded).replace(/\\/g, "");
         alert(result);
         //if (response.data.logged_in) {
           //this.props.handleSuccessfulAuth(response.data);
         //}
+        localStorage.setItem("dftoken", response.result);
       })
       .catch(error => {
         console.log("login error", error);
@@ -76,7 +84,7 @@ class Login extends Component {
             type="text"
             name="username"
             placeholder="Username"
-            value={this.state.username}
+            defaultvalue={this.state.username}
             onChange={this.handleChange}
             required
           />
