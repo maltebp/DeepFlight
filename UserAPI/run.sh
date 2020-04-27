5c4a2e7fec1f0e04e128ccb72a0ed82da8ce5165
maltebp <maltebp94@gmail.com>
2020-04-27 21:01:08 +0200

    Updated git ignore

--- UserAPI/run.sh
+++ UserAPI/run.sh
@@ -5,8 +5,8 @@
 target=./DeepFlight/UserAPI
 
 mvn clean package &&\
-scp -r -i "$AMAZON_KEY" Dockerfile target/UserAPI.jar ec2-user@maltebp.dk:./DeepFlight/UserAPI &&\
-ssh -i "$AMAZON_KEY" ec2-user@maltebp.dk "cd ./DeepFlight/UserAPI && docker build -t deepflight/userapi . && docker run -d -p 7000:7000 deepflight/userapi"
+scp -r -i "$AMAZON_KEY" Dockerfile startContainer.sh target/UserAPI.jar ec2-user@maltebp.dk:./DeepFlight/UserAPI &&\
+ssh -i "$AMAZON_KEY" ec2-user@maltebp.dk "cd ./DeepFlight/UserAPI && ./startContainer.sh"
 
 #scp -r -i "$AMAZON_KEY" pom.xml Dockerfile src ec2-user@maltebp.dk:./DeepFlight/GameAPI
 #scp -r -i "$AMAZON_KEY" src/main/java/* ec2-user@maltebp.dk:./DeepFlight/GameAPI/
