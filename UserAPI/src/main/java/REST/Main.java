f6e7ab88780ba825b2dd8725dd071d9ecbaddc62
AndreasBGJensen <bgandreas@hotmail.com>
2020-04-28 20:09:12 +0200

    Add: Add test and HandlerLogin and comments

--- UserAPI/src/main/java/REST/Main.java
+++ UserAPI/src/main/java/REST/Main.java
@@ -2,19 +2,18 @@ package REST;
 
 import Controller.Authendicator;
 import JWT.JWTHandler;
+
+
+import Respons.Response;
+import Respons.ResponseText;
 import brugerautorisation.data.Bruger;
 
-import brugerautorisation.data.UserPass;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.javalin.Javalin;
-import io.javalin.http.Handler;
 import javalinjwt.JavalinJWT;
-import javalinjwt.examples.JWTResponse;
 
-import java.io.IOException;
-import java.util.Iterator;
 import java.util.Optional;
 
 public class Main {
@@ -35,6 +34,7 @@ public class Main {
 
 
         app.before("/jwt/*", ctx -> {
+
             String source = "Authfilter";
 
             Optional<DecodedJWT> decodedJWT = JavalinJWT.getTokenFromHeader(ctx)
@@ -51,11 +51,12 @@ public class Main {
 
 
         /*
-        Response message for no access token.
+        Respons.Respons message for no access token.
          */
         app.get("/loginRequest",ctx->{
             ctx.status(401);
-            ctx.result("Please provide login information:\n POST information to /login\n The format should be: 'name':'username' 'password': 'password'\n\nThis game service is arthendicated througt javabog.dk. Please contact them if you have problems with authendication ");
+            //ctx.result("Please provide login information:\n POST information to /login\n The format should be: 'name':'username' 'password': 'password'\n\nThis game service is arthendicated througt javabog.dk. Please contact them if you have problems with authendication ");
+            ctx.result(Response.generateResponse(ResponseText.LOGIN_INFO_RESPONCE,null));
         });
 /*
 #####################################################################################################################################
@@ -70,87 +71,19 @@ public class Main {
         Rest endpoint: param: logininformation
         response: token String.
          */
-        app.post("/login", ctx -> {
-
-          try {
-              System.out.println("Endpoint: login");
-              String name = ctx.formParam("name");
-              String pwd = ctx.formParam("password");
-
-              System.out.println(name + ",pwd " + pwd); //For debugging
-              Bruger user = Authendicator.Authendication(name, pwd);
-              if (user == null) {
-                  ctx.result("Un authorized");
-              } else {
-                  String token = JWTHandler.provider.generateToken(user);
-                  ctx.json(new JWTResponse(token));
-              }
-          }catch (IllegalArgumentException e){
-              ctx.result("Could not fin user");
-          }
-        });
-
+        app.post("/login",LoginHandler.login);
 
         /*
         Endpoint for changing password
         */
+        app.post("/jwt/changeLogin",LoginHandler.changePassword);
 
-        app.post("/jwt/changeLogin", ctx->{
-            System.out.println("Endpoint: /login/changeLogin");
-            try {
-                String pwd = ctx.formParam("new_password");
-
-                ObjectMapper mapper = new ObjectMapper();
-                Bruger bruger = mapper.readValue(unpactkToken(ctx,"user"), Bruger.class);
-                System.out.println(bruger.toString());
-
-
-                String username = bruger.brugernavn;
-                String password = ctx.formParam("password");
-                System.out.println(pwd+username+password); //For debugging
-
-                Bruger newUser = Authendicator.AuthChangePassword(username, password, pwd);
-
-                if (newUser == null) {
-                    ctx.status(500);
-                    ctx.result("Internal server error");
-                }
-
-                ctx.result("Sucessfully changed password");
-
-            }catch (Exception e){
-                e.printStackTrace();
-                ctx.status(400);
-                ctx.result("Missing param");
-            }
-        });
-        /*
-####################################################RESOLVE ENDPOINT#################################################################################
- */
 
         //Get user info in exchange for JWT.
-        app.get("/jwt/exchangeUser", ctx->{
-
-            ctx.result(unpactkToken(ctx,"user"));
-        });
-
-
-
+        app.get("/jwt/exchangeUser",LoginHandler.exchangeUser );
 
     }
-/*
-####################################################HELPER FUNCTIONS#################################################################################
- */
 
-    /*
-    A clumsy way of unpacking tokens
-     */
 
-    private static String unpactkToken(io.javalin.http.Context ctx, String infoToRetrive){
-        Optional<DecodedJWT> decodedJWT = JavalinJWT.getTokenFromHeader(ctx)
-                .flatMap(JWTHandler.provider::validateToken);
 
-        return decodedJWT.get().getClaim(infoToRetrive).asString();
-
-    }
 }
