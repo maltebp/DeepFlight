572068f569b8070ef54a8b99fc8f8c4f41163ce8
ErlendTyrmi <erlend@tyrmi.com>
2020-04-28 17:54:12 +0200

    branching on dl page

--- WebSite/src/Download.js
+++ WebSite/src/Download.js
@@ -3,15 +3,63 @@ import axios from 'axios';
 import jwt from 'jsonwebtoken';
 
 class Download extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      token: localStorage.getItem("dftoken"),
+    }
+  }
+
+  handleState(){
+    this.setState({
+      token: localStorage.getItem("dftoken"),
+    })
+    console.log("Handlin' state");
+    console.log("Token in Parent: " + JSON.stringify(this.state.token))
+  }
+
   render() {
     return (
       <div>
-        <h2>LOGIN TO DOWNLOAD THE GAME!</h2>
+        <h2>DOWNLOAD THE GAME!</h2>
         <div className="boxwrapper">
-        <div className="box">
-        <Login/>
+          <FilterDownload token={this.state.token} handleState={this.handleState.bind(this)}/>
+          <div className="box whitebg">
+            <h2>Installation</h2>
+            <h3>Windows</h3>
+            <p>Instructions</p>
+            <h3>Mac</h3>
+            <p>Instructions</p>
+            <h3>Linux</h3>
+            <p>Instructions</p>
+          </div>
         </div>
-        </div>
+      </div>
+    );
+  }
+}
+
+// Show the login menu of the user has no token
+// Show download text box if logged in
+function FilterDownload(props) {
+  console.log("Token in FilterDownload: " + JSON.stringify(props.token));
+  if (props.token == 'undefined' || props.token == null) {
+    return (<Login handleState={props.handleState}/>);
+  } else {
+    // Try to login automatically first // TODO: Show 'token expired' if old token
+    return (<DownloadBox />)
+  }
+}
+
+class DownloadBox extends React.Component {
+  DownloadGame() {
+    console.log("Starting download")
+  }
+  render() {
+    return (
+      <div className="box whitebg">
+        <p>You are logged in and can download the link from here.</p>
+        <button onClick="DownloadGame">DL GME</button>
       </div>
     );
   }
@@ -21,7 +69,6 @@ class Download extends Component {
 class Login extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       name: "",
       password: "",
@@ -30,6 +77,7 @@ class Login extends Component {
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleState = this.props.handleState;
   }
 
   handleChange(event) {
@@ -55,20 +103,24 @@ class Login extends Component {
         'Access-Control-Allow-Origin': 'http://maltebp.dk:7000/login'
         //'Access-Control-Allow-Origin': 'http://localhost:7000'
       },
-      withCredentials: true})
+      withCredentials: true
+    })
 
       .then(response => {
         // TODO remove these alerts and logs!
-        console.log(response);
+        //console.log(response);
         const token = response.data;
         var decoded = jwt.decode(token.jwt);
-        console.log(decoded);
+        //console.log(decoded);
         var result = JSON.stringify(response.statusText) + "\nJWT:\n" + JSON.stringify(decoded).replace(/\\/g, "");
         alert(result);
         //if (response.data.logged_in) {
-          //this.props.handleSuccessfulAuth(response.data);
+        //this.props.handleSuccessfulAuth(response.data);
         //}
-        localStorage.setItem("dftoken", response.result);
+        localStorage.setItem("dftoken", token);
+        console.log("Token in child " + token);
+        console.log("Read from localStorage" + localStorage.getItem("dftoken"));
+        this.handleState();
       })
       .catch(error => {
         console.log("login error", error);
@@ -78,13 +130,13 @@ class Login extends Component {
 
   render() {
     return (
-      <div>
+      <div className="box">
         <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             name="username"
             placeholder="Username"
-            defaultvalue={this.state.username}
+            defaultValue={this.state.username}
             onChange={this.handleChange}
             required
           />
@@ -97,7 +149,6 @@ class Login extends Component {
             onChange={this.handleChange}
             required
           />
-
           <button type="Login">Login</button>
         </form>
       </div>
