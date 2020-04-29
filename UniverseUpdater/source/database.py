39f5f532d3f2309620eee6e5d90df7d4240ec5bd
maltebp <maltebp94@gmail.com>
2020-04-29 22:29:49 +0200

    UniverseUpdate: setup some more stuff

--- UniverseUpdater/source/database.py
+++ UniverseUpdater/source/database.py
@@ -1,4 +1,5 @@
 import pymongo
+import bson
 
 from source.track import Track
 
@@ -6,7 +7,9 @@ from source.track import Track
 _use_test_database = True
 _db_user = "universeupdater"
 _db_password = "deepflightisawesome"
-_db_name = "gamedb"
+_db_name = "game"
+
+
 
 
 # ----------------------------------------------------------------------------------
@@ -15,6 +18,7 @@ _db_name = "gamedb"
 # Establish connection
 client = pymongo.MongoClient(f"mongodb+srv://{_db_user}:{_db_password}@deepflight-cu0et.mongodb.net/test?retryWrites=true&w=majority")
 db = client["gamedb" + ("_test" if _use_test_database else "")]
+print(f"Collections: {db.list_collection_names()}")
 
 
 def get_planets():
@@ -22,9 +26,20 @@ def get_planets():
 
 
 def add_track(track):
-    print("Saving track")
+    db_track = {"name": track.name, "planetId": track.planetId}
+
+    # Return value of insert is the _id generated
+    _id = db["tracks"].insert_one(db_track).inserted_id
+    print("_id" + str(_id))
+
+    if track.data is not None:
+        print("Track data is not None")
+        db["trackdata"].insert_one({"_id": _id, "data": bson.Binary(track.data)})
 
 
+def get_track_data():
+    data = db["trackdata"].find()
+    print("Documents in 'trackdata': " + str(data.count()))
 
 def get_tracks():
     db_tracks = db["tracks"].find()
@@ -32,6 +47,7 @@ def get_tracks():
     # Convert to Track objects
     tracks = []
     for db_track in db_tracks:
+        print("a")
         track = Track(db_track["_id"], db_track["name"], db_track["planetId"] )
         tracks.append(track)
 
--- UniverseUpdater/source/main.py
+++ UniverseUpdater/source/main.py
@@ -1,12 +1,20 @@
 
 
 from source import database
+from source.track import Track
 
 print("Starting Universe Update")
 
 planets = database.get_planets()
 
+database.get_track_data()
+
 print(*planets, sep="\n")
+
+track = Track(None, "My Planet", 1)
+track.data = "Hello World!".encode()
+print(track.data)
+database.add_track(track)
 
 
 tracks = database.get_tracks()
