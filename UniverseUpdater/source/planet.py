7424c25401477db07e649bcd6d2bab0d2bbe0d6d
maltebp <maltebp94@gmail.com>
2020-04-30 11:11:16 +0200

    UniversalPython: Setup track generation

---
+++ UniverseUpdater/source/planet.py
@@ -0,0 +1,14 @@
+
+class Planet:
+
+    def __init__(self, id, name, color, lengthFactor, curveFactor, stretchFactor, widthFactor, widthNoiseFactor):
+        self.id = id
+        self.name = name
+        self.color = color
+        self.lengthFactor = lengthFactor
+        self.curveFactor = curveFactor
+        self.stretchFactor = stretchFactor
+        self.widthFactor = widthFactor
+        self.widthNoiseFactor = widthNoiseFactor
+
+
