58b5cd32bd1b4746865eec11924fd445d4ead4fd
Erlend Tyrmi <erlend@tyrmi.com>
2020-05-04 11:50:38 +0200

    Expiration date for jwt 12 minutes

--- UserAPI/src/main/java/JWT/JWTHandler.java
+++ UserAPI/src/main/java/JWT/JWTHandler.java
@@ -5,11 +5,12 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javalinjwt.JWTGenerator;
 import javalinjwt.JWTProvider;
+
+import java.util.Date;
 
 public class JWTHandler {
 
@@ -22,8 +23,7 @@ public class JWTHandler {
      */
 
     //1.
-   static Algorithm algorithm = Algorithm.HMAC256("very_secret");
-
+    static Algorithm algorithm = Algorithm.HMAC256("very_secret");
     //2.
     /*
     For generating token
@@ -31,36 +31,31 @@ public class JWTHandler {
     static JWTGenerator<Bruger> generator = (user, alg) -> {
 
         //Remove password from userobject
-        user.adgangskode="";
+        user.adgangskode = "";
+        Date expiration = new Date(System.currentTimeMillis() + 12 * 60 * 1000); // + 12 minutes
 
         ObjectMapper mapper = new ObjectMapper();
         try {
             String jsonUser = mapper.writeValueAsString(user);
 
-        JWTCreator.Builder token = JWT.create()
-                .withClaim("user", jsonUser);
-        return token.sign(alg);
+            JWTCreator.Builder token = JWT.create()
+                    .withClaim("user", jsonUser)
+                    .withExpiresAt(expiration);
+            return token.sign(alg);
 
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
         return null;
     };
-
     //3.
     /*
-    For verifeing JWT
+    For verifying JWT
      */
     public static JWTVerifier verifier = JWT.require(algorithm).build();
-
     //4.
     /*
     The wrapper object is created
      */
     public static JWTProvider provider = new JWTProvider(algorithm, generator, verifier);
-
-
-
-
-
 }
