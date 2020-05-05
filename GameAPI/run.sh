0200ccf5551dfdb339f7d6bb5e50d2434764cfbd
Erlend Tyrmi <erlend@tyrmi.com>
2020-05-06 00:05:31 +0200

    Game dl (#15)
    * Add: Exception handling from javabog backend (IllegalArgumentException)
    
    * Add: Exception handling from javabog backend (IllegalArgumentException)
    
    * Add: user ObjectMapper
    
    * Add: debug comment
    
    * Updated git ignore
    
    * Updated gitignore to ignore idea folder
    
    * Added git attributes for LF conversion
    
    * GameAPI: Setup user resource (for authentication as well)
    
    * GameAPI: random stuff
    
    * UniverseUpdater: Added universeupdater
    
    * started work on DownloadService
    
    * started work on DownloadService
    
    * started work on DownloadService
    
    * started work on DownloadService
    
    * started work on DownloadService
    
    * started work on DownloadService
    
    * started work on DownloadService
    
    * login/download almost done
    
    Co-authored-by: AndreasBGJensen <bgandreas@hotmail.com>
    Co-authored-by: maltebp <maltebp94@gmail.com>

--- GameAPI/run.sh
+++ GameAPI/run.sh
@@ -5,9 +5,9 @@
 mvn clean package &&\
 scp -r -i "$AMAZON_KEY" Dockerfile target/GameAPI.jar startContainer.sh ec2-user@maltebp.dk:./DeepFlight/GameAPI &&\
 
-ssh -i "$AMAZON_KEY" ec2-user@maltebp.dk "cd DeepFlight/GameAPI && ./startContainer.sh"
+ssh -i "$AMAZON_KEY" ec2-user@maltebp.dk "cd DeepFlight/server.GameAPI && ./startContainer.sh"
 
-#scp -r -i "$AMAZON_KEY" pom.xml Dockerfile src ec2-user@maltebp.dk:./DeepFlight/GameAPI
-#scp -r -i "$AMAZON_KEY" src/main/java/* ec2-user@maltebp.dk:./DeepFlight/GameAPI/
+#scp -r -i "$AMAZON_KEY" pom.xml Dockerfile src ec2-user@maltebp.dk:./DeepFlight/server.GameAPI
+#scp -r -i "$AMAZON_KEY" src/main/java/* ec2-user@maltebp.dk:./DeepFlight/server.GameAPI/
 
 # && ssh -i "$AMAZON_KEY" ec2-user@maltebp.dk "cd /home/ec2-user/HangmanOnline && ./dbapi_start.sh"
