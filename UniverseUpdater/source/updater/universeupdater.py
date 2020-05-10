5ede566c1756e4851dac596e4e7053a88767a204
maltebp <maltebp94@gmail.com>
2020-05-10 14:57:52 +0200

    UniverseUpdater: Updated Round length

--- UniverseUpdater/source/updater/universeupdater.py
+++ UniverseUpdater/source/updater/universeupdater.py
@@ -11,7 +11,7 @@ from prometheus_client import start_http_server
 start_http_server(8000)
 
 _UPDATE_FREQUENCY = 30 # seconds
-_ROUND_LENGTH = 10 # Minutes
+_ROUND_LENGTH = 1440 # Minutes
 _CLEAR_DATABSE_ON_START = False
 _TEST_MODE = False
 
