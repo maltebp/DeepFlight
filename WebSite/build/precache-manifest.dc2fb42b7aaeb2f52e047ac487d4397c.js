3cd368596439e3daacb2e33555709df3c4f445e4
ErlendTyrmi <erlend@tyrmi.com>
2020-05-11 23:26:56 +0200

    removed tables in plasnets.js

---
+++ WebSite/build/precache-manifest.dc2fb42b7aaeb2f52e047ac487d4397c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6467bb9f33ec5e51d5d12dad5fa3fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19b7818b05440884a9d2",
+    "url": "/static/css/main.477329c3.chunk.css"
+  },
+  {
+    "revision": "a94dea579143409c0d74",
+    "url": "/static/js/2.8df44d24.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.8df44d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b7818b05440884a9d2",
+    "url": "/static/js/main.5e1d80a4.chunk.js"
+  },
+  {
+    "revision": "4158ac17a1d32ace7713",
+    "url": "/static/js/runtime-main.bb9064c6.js"
+  },
+  {
+    "revision": "cf3d8a1d02b1ae59eb799c5305c73cfa",
+    "url": "/static/media/CircleLupto.cf3d8a1d.png"
+  },
+  {
+    "revision": "df258c595d7acbcf61f1a18bb2bc6eb6",
+    "url": "/static/media/CircleSmar.df258c59.png"
+  },
+  {
+    "revision": "496f5330d2c390d6487c296d2c550426",
+    "url": "/static/media/Pixellari.496f5330.ttf"
+  },
+  {
+    "revision": "105a429e7fe44e59f56414f2dd3eb27d",
+    "url": "/static/media/df.105a429e.jpg"
+  },
+  {
+    "revision": "1d367245376e9bcc501aff8d41bdceef",
+    "url": "/static/media/screenshot.1d367245.jpg"
+  },
+  {
+    "revision": "7c2018eb4458cfeabdf9e2b964bdff4b",
+    "url": "/static/media/screenshot2.7c2018eb.png"
+  },
+  {
+    "revision": "ac34718b0a2565dd796c9dcadec60b01",
+    "url": "/static/media/screenshot3.ac34718b.png"
+  }
+]);
